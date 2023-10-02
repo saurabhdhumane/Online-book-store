3c371420762098c9c7e35134e319edbd17692693
Saurabh Dhumane <saurabhdhumneraje@gmail.com>
2023-10-02 16:25:04 +0530

    Add files via upload

---
+++ src/User.java
@@ -0,0 +1,13 @@
+// class "User" start
+
+class User {
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
+
+// class "User" end
